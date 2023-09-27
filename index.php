615cf6e49906198af297a284c26dc9755370c71a
Tiago Bezerra dos Santos <bsantostiago@gmail.com>
2023-09-27 11:22:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,31 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 8</title>
+    <title>Exemplo 9</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 8</h1>
+    <h1>PHP com POO - Exemplo 9</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Agrupamento e organização de recursos (classes, funções, constantes)</li>
-        <li>Prevenção de conflitos entre classes de mesmo nome</li>
-        <li>Definição e uso de namespaces e alias</li>
+        <li>Composer: gerenciamento de dependências</li>
+        <li>Autoload de classes</li>
     </ul>
 
     <h3>EXEMPLO 1 (SEM MEXER NAS CLASSES EXISTENTES)</h3>
 
 <?php
-// Só de importar, já dá erro (antes de por namespace)
 require_once "src/fornecedores/Pagamento.php";
 require_once "src/prestadores/Pagamento.php";
 
-// Agora pra usar e instaciar, chamamos o namespace
-// Forma 1 (Namespace\NomeDaClasse):
-// $pagamentoFornecedor = new Fornecedor\Pagamento;
-// $pagamentoPrestador = new Prestador\Pagamento;
-
-// Forma 2 (use dos namespaces e alias)
 use Fornecedor\Pagamento;
 use Prestador\Pagamento as PrestadorPagamento; // as -> apelido
 $pagamentoFornecedor = new Pagamento;
@@ -41,13 +33,6 @@ $pagamentoPrestador = new PrestadorPagamento;
 
 <h3>EXEMPLO 2 (NAMESPACE NAS CLASSES EXISTENTES)</h3>
 <?php
-
-// use Tabajara\PessoaFisica as PF;
-// use Tabajara\PessoaJuridica as PJ;
-// use Tabajara\MEI;
-// use Tabajara\Utilitarios;
-
-// ou assim:
 use Tabajara\{PessoaFisica as PF, PessoaJuridica as PJ, MEI, Utilitarios};
 
 require_once "src/PessoaFisica.php";
